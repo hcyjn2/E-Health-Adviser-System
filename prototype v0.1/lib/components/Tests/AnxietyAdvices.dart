31a08d51861e0428da699eeec1a597cf72d0d7c9
Mohanad31 <hcyma4@nottingham.edu.my>
2021-02-20 23:49:09 +0200

    Update AnxietyAdvices.dart

--- prototype v0.1/lib/components/Tests/AnxietyAdvices.dart
+++ prototype v0.1/lib/components/Tests/AnxietyAdvices.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 class AnxietyAdvices {
   static final AnxietyAdvices _instance = AnxietyAdvices._internal();
-  static List<String> _advices = [];
+  static Map<String, String> _advices = Map();
   factory AnxietyAdvices() {
     return _instance;
   }
@@ -22,10 +22,26 @@ class AnxietyAdvices {
   }
 
   AnxietyAdvices._internal() {
-    _advices.add('Take some deep breaths.');
-    _advices.add(
-        'Try and follow the 3-3-3 rule, name three things you see, three things you hear, and three parts of your body.');
-    _advices.add('Do not consume any sugar.');
-    _advices.add('Focus on the present.');
+    _advices['Follow the 3-3-3 rule'] =
+        "Try and follow the 3-3-3 rule, name three things you see, three things you hear, and three parts of your body.";
+    _advices['Breathing exercise'] = "Take some deep breaths.";
+    _advices['Reduce sugar intake'] =
+        "Do not consume any sugar as it increases the severity of the symptoms.";
+    _advices['Focus on the present.'] =
+        "Dwelling on the future and the past are impractical and will only cause the feelings of anxiety to worsen.";
+    _advices['Have a balanced diet'] =
+        "The body needs valuable nutrients so do not skip any meals. Try to keep energy-boosting healthy snacks.";
+    _advices['Reduce Alcohol and Caffeine Consumption'] =
+        "Both alcohol and caffeine aggravate and worsen the symptoms as well as trigger panic attacks.";
+    _advices['Discover what triggers your anxiety'] =
+        "Keeping a journal for when you are stressed and anxious will help in getting to know what causes your anxiety in order for you to avoid it.";
+    _advices['Let go'] =
+        "Accepting the fact that you cannot control everything will give you a sense of more control over your life.";
+    _advices['Exercise'] =
+        "It is proven that exercise help calm down and lessen the symptoms of anxiety. Set small daily goals and go for a jog or walk.";
+    _advices['Talk with someone'] =
+        "Talk about what is on your mind with someone you trust such as a friend or family member.";
+    _advices['Meditate'] =
+        "Guided meditation is proven to help you cope with anxiety.";
   }
 }

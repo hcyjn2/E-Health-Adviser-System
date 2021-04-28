faaaf33a52e214f14f71d2847b4cd95f970b367d
Grisha322 <grishakirpa@gmail.com>
2021-04-28 15:29:01 +0800

    resource page by Junnete

---
+++ prototype v0.1/lib/Screens/Login/components/loginbody.dart
@@ -0,0 +1,144 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:main_menu/Screens/ForgetPassword/forgotpw.dart';
+import 'package:main_menu/components/LoginSignupWelcome/EmailField.dart';
+import 'package:main_menu/components/LoginSignupWelcome/LoginCheckAcc.dart';
+import 'package:main_menu/components/LoginSignupWelcome/PasswordField.dart';
+import 'package:main_menu/components/LoginSignupWelcome/buttons.dart';
+import 'package:main_menu/screens/Login/components/loginbg.dart';
+import 'package:main_menu/screens/SignUp/signupscreen.dart';
+
+class LoginBody extends StatefulWidget {
+  LoginBody({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _LoginBodyState createState() => _LoginBodyState();
+}
+
+class _LoginBodyState extends State<LoginBody> {
+  String _email;
+  String _password;
+
+  void _loginButtonOnPressed() async {
+    if (_email != null && _password != null) {
+      try {
+        UserCredential userCredential = await FirebaseAuth.instance
+            .signInWithEmailAndPassword(email: _email, password: _password);
+        if (userCredential != null) {
+          Navigator.pushReplacementNamed(context, '/mainmenu');
+        }
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'user-not-found') {
+          {
+            return showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text(
+                      'No user found for that email.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontFamily: 'DejaVuSerif',
+                          fontWeight: FontWeight.normal),
+                    ),
+                  );
+                });
+          }
+        } else if (e.code == 'wrong-password') {
+          return showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text(
+                    'Invalid email/password. \n Kindly try again! :)',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: 'DejaVuSerif',
+                        fontWeight: FontWeight.normal),
+                  ),
+                );
+              });
+        }
+      }
+    } else {
+      return showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text(
+                'Invalid email/password.\n Please try again.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'DejaVuSerif',
+                    fontWeight: FontWeight.normal),
+              ),
+            );
+          });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LoginBG(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 3),
+          LoginInputField(
+            hintText: "Email",
+            onChanged: (value) {
+              _email = value;
+            },
+          ),
+          LoginPWField(
+            onChanged: (value) {
+              _password = value;
+            },
+          ),
+          SizedBox(height: 15),
+          LoginSignUpButtons(
+            text: "LOGIN",
+            press: _loginButtonOnPressed,
+          ),
+          SizedBox(height: 15),
+          LoginCheckAccount(
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return SignUpScreen();
+                  },
+                ),
+              );
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                child: Text(
+                  'Forgot Password?',
+                  style: const TextStyle(
+                    fontSize: 15,
+                    fontFamily: 'DejaVuSerif',
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+                onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => ForgotPasswordScreen())),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- prototype v0.1/lib/Screens/MainMenu/MainMenuDrawer.dart
+++ prototype v0.1/lib/Screens/MainMenu/MainMenuDrawer.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:main_menu/Screens/Welcome/welcomescreen.dart';
 import 'package:main_menu/components/ReferencesPage/referencescreen.dart';
-
 
 class MainMenuDrawer extends StatefulWidget {
   @override
@@ -10,66 +9,68 @@ class MainMenuDrawer extends StatefulWidget {
 }
 
 class _MainMenuDrawerState extends State<MainMenuDrawer> {
- Future <void> _signOutButtonOnPressed() async {
+  Future<void> _signOutButtonOnPressed() async {
     await FirebaseAuth.instance.signOut();
-    if(FirebaseAuth.instance.currentUser==null){
-              //if user logged out then back to main page
-              //back to first page
-              Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => WelcomeScreen()), (route) => false);
-            }
-          }
-          @override
-          Widget build(BuildContext context) {
-            return Drawer(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: <Widget>[
-                  DrawerHeader(
-                    decoration: BoxDecoration(color: Color(0xFF99d8e8)),
-                    child: Column(
-                      children: <Widget>[
-                        CircleAvatar(
-                          radius: 50.0,
-                          //image used is a temporary placeholder image
-                          backgroundImage: AssetImage('Images/cat21.png'),
-                        ),
-                        Text(
-                          'Insert Name Here',
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
-                    ),
+    if (FirebaseAuth.instance.currentUser == null) {
+      //if user logged out then back to main page
+      //back to first page
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => WelcomeScreen()),
+          (route) => false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(color: Color(0xFF99d8e8)),
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  //image used is a temporary placeholder image
+                  backgroundImage: AssetImage('Images/cat21.png'),
+                ),
+                Text(
+                  'Insert Name Here',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
                   ),
-                  ListTile(
-                    leading: Icon(Icons.settings),
-                    title: Text('Setting'),
-                    //This is for handling setting of notification
-                    onTap: () {
-                      print("Go into setting");
-                    },
-                  ),
-                  ListTile(
-                      leading: Icon(Icons.all_inbox_rounded),
-                      title: Text('Resources'),
-                      //put in references that we have used
-                      onTap: () {
-                        Navigator.push(
-                        context,
-                       MaterialPageRoute(
-    	                builder: (context) => ReferenceScreen()),
-                       );
-                      }
-                      ),
-                  ListTile(
-                      leading: Icon(Icons.arrow_back),
-                      title: Text('Logout'),
-                      onTap: () => _signOutButtonOnPressed(),
-                  ),
-                ],
-              ),
-            );
-          }
-        }
+                )
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Setting'),
+            //This is for handling setting of notification
+            onTap: () {
+              print("Go into setting");
+            },
+          ),
+          ListTile(
+              leading: Icon(Icons.all_inbox_rounded),
+              title: Text('Resources'),
+              //put in references that we have used
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ReferenceScreen()),
+                );
+              }),
+          ListTile(
+            leading: Icon(Icons.arrow_back),
+            title: Text('Logout'),
+            onTap: () => _signOutButtonOnPressed(),
+          ),
+        ],
+      ),
+    );
+  }
+}
